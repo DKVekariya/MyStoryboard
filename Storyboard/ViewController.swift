f54753ec971200d8b57647546066370c03dcf58f
Divyesh Vekariya <170543109018@darshan.ac.in>
2021-03-03 18:46:49 +0530

    Removed Unused Code

--- Storyboard/ViewController.swift
+++ Storyboard/ViewController.swift
@@ -12,28 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let gesture = UISwipeGestureRecognizer(target: self, action: #selector(onSwipe(sender:)))
-        gesture.direction = [.up, .down]
-        view.addGestureRecognizer(gesture)
-    }
-    
-    @objc func onSwipe(sender:UISwipeGestureRecognizer) {
-        present(UIViewController(), animated: true, completion: nil)
-        
-        switch sender.state {
-        case .possible:
-            <#code#>
-        case .began:
-            <#code#>
-        case .changed:
-            <#code#>
-        case .ended:
-            <#code#>
-        case .cancelled:
-            <#code#>
-        case .failed:
-            <#code#>
-        }
     }
 
 
